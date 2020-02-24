b26f381ce98f979f263227b2d1e646b83b855393
hajzeer <krzysh98@gmail.com>
2020-02-24 19:19:33 +0100

    CODA_website react 1.8.1

--- src/Menu.js
+++ src/Menu.js
@@ -33,11 +33,11 @@ class Menu extends Component {
                         <ul className="list-1">
                             <li>Grupa CODA Kamil Rzychoń</li>
                             <br/>
-                            <li>ul. Bolesłąwa Chrobrego 1/1A<br/>41-500 Chorzów</li>
+                            <li>ul. Bolesława Chrobrego 1/1A<br/>41-500 Chorzów</li>
                             <br/>
                             <li>Kamil Rzychoń</li>
                             <li>kamil@grupacoda.pl</li>
-                            <li>+48 518 781 784</li>
+                            <li>+48 518781784</li>
                         </ul>
                     </div>
                 </Fade>
